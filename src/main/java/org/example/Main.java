2724f0663987b2e1acc9cefc5a5ba0a94d3fff62
sragavendran <71277427+sragavendran@users.noreply.github.com>
2023-04-06 13:59:03 +0530

    Print Changes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,6 @@ package org.example;
 import org.apache.commons.csv.CSVFormat;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! Again1111212 Rassga11v");
+        System.out.println("Hello world!");
     }
 }
